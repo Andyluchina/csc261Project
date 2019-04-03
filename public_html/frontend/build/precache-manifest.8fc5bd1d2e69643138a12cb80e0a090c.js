8bcfcc2bffca7dfbc10872750a1588c9a241d066
Andy <qingjielu990916@gmail.com>
2019-04-03 16:24:53 -0400

    add build folder

---
+++ public_html/frontend/build/precache-manifest.8fc5bd1d2e69643138a12cb80e0a090c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e89e47ac1162ce059c49",
+    "url": "/static/js/main.4dce95c9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e89e47ac1162ce059c49",
+    "url": "/static/css/main.216a3858.chunk.css"
+  },
+  {
+    "revision": "3fed699dcc09c8797b69f1a22814278a",
+    "url": "/index.html"
+  }
+];
