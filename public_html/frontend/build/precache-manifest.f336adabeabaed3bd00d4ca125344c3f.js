e3da2f107508d2a4ffb7bc9715deface6d6dc8ba
Andy <qingjielu990916@gmail.com>
2019-04-05 23:20:11 -0400

    j

---
+++ public_html/frontend/build/precache-manifest.f336adabeabaed3bd00d4ca125344c3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "530ba03bf18d3351bc6a",
+    "url": "/static/js/main.d91a006d.chunk.js"
+  },
+  {
+    "revision": "cb85fdb9693d89b65416",
+    "url": "/static/js/2.138300e3.chunk.js"
+  },
+  {
+    "revision": "cb85fdb9693d89b65416",
+    "url": "/static/css/2.beb6da08.chunk.css"
+  },
+  {
+    "revision": "56a260c41d32aa20ef92ece6ee99d068",
+    "url": "/index.html"
+  }
+];
