a1af4e0e728e68e68573a2f659fa7a72bbdba42f
MSwanson <swanson.matthew@pacbell.net>
2019-04-23 17:52:44 -0400

    test

---
+++ public_html/ATR_route.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('db_setup.php');
+
+$sql = "USE mswanso2_1;";
+if ($conn->query($sql) === TRUE) {
+         // echo "using Database mswanso2_1";
+} else {
+	//echo "Error using  database: " . $conn->error;
+}
+
+$json=file_get_contents('php://input');
+$stuff=array();
+$data=json_decode($json);
+$sql="SELECT TITLE FROM EMPLOYEE WHERE WORK_ID=".$data->workid;
+$result2 = $conn->query($sql);
+$check = $result2->fetch_assoc();
+
+$tablename=$data->tablename;
+
+if($check['TITLE']=='Administrator'){
+    $sql="select column_name from information_schema.columns where table_name=$tablename;";
+    $result = $conn->query($sql);
+    if($result==TRUE){
+        $row=$result3->fetch_assoc();
+        $stuff[]=$row;
+        echo json_encode($stuff);
+    }
+    else{
+        print("Error");
+    }
+}
+
+
+$conn->close();
+?>
