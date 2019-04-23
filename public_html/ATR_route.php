2a6c33ab04b37de3ce536d7bb5c4bded05e22d0e
MSwanson <swanson.matthew@pacbell.net>
2019-04-23 18:56:54 -0400

    test

--- public_html/ATR_route.php
+++ public_html/ATR_route.php
@@ -19,7 +19,7 @@ $check = $result2->fetch_assoc();
 $tablename=$data->tablename;
 
 if($check['TITLE']=='Administrator'){
-    $sql="select column_name from information_schema.columns where table_name='$tablename';";
+    $sql="select column_name from information_schema.columns where table_name=".$tablename.";";
     if($tablename==''){
         $stuff[]=[];
         echo json_encode($stuff);
