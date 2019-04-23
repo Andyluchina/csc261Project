7add8e1fbef5e107653958ac0bd637665a984efd
MSwanson <swanson.matthew@pacbell.net>
2019-04-23 19:29:39 -0400

    test

--- public_html/ATR_route.php
+++ public_html/ATR_route.php
@@ -16,24 +16,27 @@ $data=json_decode($json);
 
 $tablename=$data->tablename;
 
-    $sql="select column_name from information_schema.columns where table_name='$tablename';";
-    $stuff[]=$sql;
-    echo json_encode($sql);
-    if($tablename==''){
-        echo json_encode($stuff);
-    }
-    else{
-        $result = $conn->query($sql);
-        if($result==TRUE){
-            $stuff[]=$row;
-            echo json_encode($stuff);
-        }
-        else{
-            $stuff[]="Error";
-            echo json_encode($stuff);
-        }
+$sql="select column_name from information_schema.columns where table_name='$tablename';";
+$stuff[]=$sql;
+echo json_encode($sql);
+if($tablename==''){
+    echo json_encode($stuff);
+}
+else{
+    $result = $conn->query($sql);
+    if($result==TRUE){
+        while($row = $result->fetch_assoc())){
+    $stuff[]=$row;
+}
 
-    }
+echo json_encode($stuff);
+}
+else{
+    $stuff[]="Error";
+    echo json_encode($stuff);
+}
+
+}
 
 
 
