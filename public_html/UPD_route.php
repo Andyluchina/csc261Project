0ba1cb11c3a50733fc9f0b00899da0026866814b
MSwanson <swanson.matthew@pacbell.net>
2019-04-24 00:11:05 -0400

    test

--- public_html/UPD_route.php
+++ public_html/UPD_route.php
@@ -8,8 +8,6 @@ function traverseArray($num2,$sqlString,$curData1,$prevData2){
 	$num=1;
 
 	foreach($curData1 as $key=>$value){
-		$value=checkIfID('$key',$value,$prevData2->{'$key'});
-
 		if($num2==1&&($value==''||$value==NULL)){
 			continue;
 		}
@@ -52,12 +50,23 @@ function traverseArray($num2,$sqlString,$curData1,$prevData2){
 	return $sqlString.$string1;
 
 }
-function checkIfID($string1,$value,$newVal){
-	if($string1=='PROJ_ID'||$string1=='WORK_ID'||$string1=='CONTRACTOR_ID'||$string1=='MISSION_ID'||$string1=='PROJ_ID'){
-		return $value;
-	}
-	return $newVal;
 
+function givePrivaleges($title, $tablename){
+  if($title=='Administrator'){
+    return 1;
+  }
+  else if(($title=='Engineer' || $title=='Mission Leader'|| $title=='Project Leader') && $tablename=='EMPLOYEE'){
+    return 2;
+  }
+  else if($title=='Mission Leader'&& $tablename!='WORKS_ON'){
+    return 1;
+  }
+  else if($title =='Project Leader'&& $tablename!='WORKS_ON' && $tablename!='MISSION'){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
 
 require_once('db_setup.php');
@@ -79,10 +88,9 @@ $check = $result1->fetch_assoc();
 $tablename=$data->tableName;
 $prev=$data->payload->pre;
 $cur=$data->payload->cur;
+$privilages=givePrivaleges($check['TITLE'],$tablename);
 
-
-
-if($check['TITLE']=='Administrator'){
+if($privilages==1){}
 	$sql="UPDATE $tablename ".traverseArray(0,"SET ",$cur,$prev)." ".traverseArray(1,"WHERE ",$prev,$prev).";";
 
 
@@ -95,8 +103,24 @@ if($check['TITLE']=='Administrator'){
 		echo json_encode($stuff);
 	}
 
-	
+}
+else if($privilages==2){
+	$sql="UPDATE EMPLOYEE ".traverseArray(0,"SET ",$cur,$prev)." ".traverseArray(1,"WHERE ",$prev,$prev).";";
 
+
+	$result3 = $conn->query($sql);
+	if ($result3 == TRUE && ($conn->affected_rows > 0) ) {
+		$stuff[]="Succesfully updated!";
+		echo json_encode($stuff);
+	} else {
+		$stuff[]="Update not successful.";
+		echo json_encode($stuff);
+	}
+
+}
+else{
+	$stuff[]="NA";
+	echo json_encode($stuff);
 
 }
 
