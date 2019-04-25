b1fa0b0eb501171c65e8059124260e8c75c24d78
MSwanson <swanson.matthew@pacbell.net>
2019-04-25 16:07:23 -0400

    test

--- public_html/schema_route.php
+++ public_html/schema_route.php
@@ -27,12 +27,30 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $json=file_get_contents('php://input');
-$shemaObj->EMPLOYEE=getAttributes('EMPLOYEE');
-$shemaObj->MISSION=getAttributes('MISSION');
+
+$tablenames=array();
+$sql="select column_name from information_schema.columns where table_name=".$tablename;
+$result = $conn->query($sql);
+if($result==TRUE){
+	while($row = $result->fetch_assoc()){
+		$tablenames[]=$row["column_name"];
+	}
+}
+else{
+	$stuff[]="Error";
+	echo json_encode($stuff);
+}
+
+$shemaObj->EMPLOYEE=$tablenames;
+
+
+
+
+/*$shemaObj->MISSION=getAttributes('MISSION');
 $shemaObj->PROJECT=getAttributes('PROJECT');
 $shemaObj->WORKS_ON=getAttributes('WORKS_ON');
 $shemaObj->CONTRACTOR=getAttributes('CONTRACTOR');
-$shemaObj->SUPPLIES=getAttributes('SUPPLIES');
+$shemaObj->SUPPLIES=getAttributes('SUPPLIES');*/
 
 
 echo json_encode($schemaObj);
