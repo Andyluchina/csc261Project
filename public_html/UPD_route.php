00f38d935984a12d9bf6fe4f841fb28631472d9b
MSwanson <swanson.matthew@pacbell.net>
2019-04-27 18:16:09 -0400

    test

--- public_html/UPD_route.php
+++ public_html/UPD_route.php
@@ -48,7 +48,7 @@ function isID($atrStr,$tablename){
 }
 function canUpdTitle($curStr,$prevStr,$title){
 
-	if(($title=='Engineer'&&($prevStr=='Engineer'||$curStr!='Engineer')) ||($title=='Project Leader'&& ($prevStr=='Project Leader'||$curStr!='Project Leader'))){
+	if(($title=='Engineer'&&($prevStr=='Engineer'&&$curStr!='Engineer')) ||($title=='Project Leader'&& ($prevStr=='Project Leader'&&$curStr!='Project Leader'))){
 		return FALSE;
 	}
 	if($title=='Mission Leader' && (($prevStr=='Administrator'||$prevStr=='Mission Leader')||($curStr=='Administrator'||$curStr=='Mission Leader'))){
@@ -125,7 +125,7 @@ if($privilages==1){
 else if($privilages==2){
 	$sql="UPDATE EMPLOYEE ".traverseArray(0,"SET ",$cur,$prev,$tablename,$check['TITLE'])." ".traverseArray(1,"WHERE ",$prev,$prev,$tablename,$check['TITLE']).";";
 
-
+	echo json_encode($sql);
 	$result3 = $conn->query($sql);
 	if ($result3 == TRUE && ($conn->affected_rows > 0) ) {
 		$stuff[]="Succesfully updated!";
