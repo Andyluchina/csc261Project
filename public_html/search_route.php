a02740f0a0438ccc0dc5e7a6d15cb0de28d9e7e0
MSwanson <swanson.matthew@pacbell.net>
2019-04-27 23:04:44 -0400

    fixed stuff

--- public_html/search_route.php
+++ public_html/search_route.php
@@ -45,11 +45,11 @@ function checkAssignable($string,$check,$boolean,$tablename){
 	$sqlStr='';
 	if($boolean && $tablename=='EMPLOYEE'){
 		if($check==1){
-			$sqlStr=$string." WORK_ID NOT IN (SELECT EMPLOYEE_ID FROM WORKS_ON) AND (TITLE!='Mission Leader' OR TITLE!='Administrator')";
+			$sqlStr=$string." WORK_ID NOT IN (SELECT EMPLOYEE_ID FROM WORKS_ON) AND TITLE!='Mission Leader' AND TITLE!='Administrator'";
 			return $sqlStr;
 		}
 		else{
-			$sqlStr=" AND WORK_ID NOT IN (SELECT EMPLOYEE_ID FROM WORKS_ON) AND (TITLE!='Mission Leader' OR TITLE!='Administrator')";
+			$sqlStr=" AND WORK_ID NOT IN (SELECT EMPLOYEE_ID FROM WORKS_ON) AND TITLE!='Mission Leader' AND TITLE!='Administrator'";
 			return $sqlStr;
 		}
 	}
