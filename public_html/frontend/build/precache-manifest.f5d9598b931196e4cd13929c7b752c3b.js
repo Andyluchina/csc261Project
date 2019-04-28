4bd484c069d2b96630a83ed0be93f595fa51e4f7
Andy <qingjielu990916@gmail.com>
2019-04-27 20:47:21 -0400

    stuff

---
+++ public_html/frontend/build/precache-manifest.f5d9598b931196e4cd13929c7b752c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c131667e67a511849fd",
+    "url": "/static/js/main.f0b28491.chunk.js"
+  },
+  {
+    "revision": "1c0d6721b6f2f4f740dc",
+    "url": "/static/js/2.60024e49.chunk.js"
+  },
+  {
+    "revision": "1c0d6721b6f2f4f740dc",
+    "url": "/static/css/2.beb6da08.chunk.css"
+  },
+  {
+    "revision": "b96cb88ec4658d5d68926dc876a5fab2",
+    "url": "/index.html"
+  }
+];
